7f5559c01047edfe0e68b053f26925b898c4af94
openmindev <boyuan.eth@gmail.com>
2025-10-11 16:49:44 -0700

    Fix lint

--- src/actions/move_go2_action/connector/unitree_sdk.py
+++ src/actions/move_go2_action/connector/unitree_sdk.py
@@ -65,18 +65,24 @@ class ActionUnitreeSDKConnector(ActionConnector[ActionInput]):
                 except Exception as e:
                     logging.error(f"Error sending ShakeHand command: {e}")
             else:
-                logging.info("ActionUnitreeSDKConnector: Still performing previous action")
+                logging.info(
+                    "ActionUnitreeSDKConnector: Still performing previous action"
+                )
         elif action == "dance":
             if self.unitree_go2_state.go2_action_progress == 0:
                 logging.info("ActionUnitreeSDKConnector: Dancing")
                 try:
                     if self.sport_client is not None:
-                        dance_move = random.choice([self.sport_client.Dance1, self.sport_client.Dance2])
+                        dance_move = random.choice(
+                            [self.sport_client.Dance1, self.sport_client.Dance2]
+                        )
                         dance_move()
                 except Exception as e:
                     logging.error(f"Error sending Dance command: {e}")
             else:
-                logging.info("ActionUnitreeSDKConnector: Still performing previous action")
+                logging.info(
+                    "ActionUnitreeSDKConnector: Still performing previous action"
+                )
         elif action == "stretch":
             if self.unitree_go2_state.go2_action_progress == 0:
                 logging.info("ActionUnitreeSDKConnector: Stretching")
@@ -86,6 +92,8 @@ class ActionUnitreeSDKConnector(ActionConnector[ActionInput]):
                 except Exception as e:
                     logging.error(f"Error sending Stretch command: {e}")
             else:
-                logging.info("ActionUnitreeSDKConnector: Still performing previous action")
+                logging.info(
+                    "ActionUnitreeSDKConnector: Still performing previous action"
+                )
         else:
             logging.warning(f"Action '{action}' not recognized or not implemented.")
