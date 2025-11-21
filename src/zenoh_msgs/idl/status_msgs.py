e5ba09ef629d88642dd7992354fc0f8607f477ce
YuchengZhou821 <yucheng@openmind.org>
2025-11-21 13:59:01 -0800

    fix lint

--- src/zenoh_msgs/idl/status_msgs.py
+++ src/zenoh_msgs/idl/status_msgs.py
@@ -148,7 +148,7 @@ class AvatarFaceResponse(IdlStruct, typename="AvatarFaceResponse"):
 class ConfigRequest(IdlStruct, typename="ConfigRequest"):
     header: Header
     request_id: String
-    config: String = String("") # ignored for GET_CONFIG
+    config: String = String("")  # ignored for GET_CONFIG
 
 
 @dataclass
