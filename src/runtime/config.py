1de324fb6edccdfe2687ee1e7dbef9a9bf4d8459
YuchengZhou821 <yucheng@openmind.org>
2026-02-18 14:58:11 -0800

    remove blank line

--- src/runtime/config.py
+++ src/runtime/config.py
@@ -560,7 +560,6 @@ def load_mode_config(
     g_robot_ip = raw_config.get("robot_ip")
     g_api_key = raw_config.get("api_key")
     g_URID = raw_config.get("URID")
-
     g_ut_eth = raw_config.get("unitree_ethernet", None)
     if g_ut_eth is None or g_ut_eth == "":
         logging.info("No robot hardware ethernet port provided.")
