f0770e74eb6998af86ed2235a9792032b9f079db
Wanbogang <harapan.bontang@gmail.com>
2026-02-21 18:43:59 +0700

    fix: force reimport zenoh_msgs from src/ in conftest.py

--- tests/zenoh_msgs/conftest.py
+++ tests/zenoh_msgs/conftest.py
@@ -6,3 +6,8 @@ src_path = os.path.join(os.path.dirname(__file__), "..", "..", "src")
 src_path = os.path.abspath(src_path)
 if src_path not in sys.path:
     sys.path.insert(0, src_path)
+
+# Remove cached zenoh_msgs from sys.modules to force reimport from src/
+for key in list(sys.modules.keys()):
+    if "zenoh_msgs" in key:
+        del sys.modules[key]
