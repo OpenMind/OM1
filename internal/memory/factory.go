656a8367c17bb48ee3ed5b8baf78dce49dafefcb
YuchengZhou821 <yucheng@openmind.org>
2026-07-01 13:37:42 -0700

    remove comment

--- internal/memory/factory.go
+++ internal/memory/factory.go
@@ -3,8 +3,6 @@ package memory
 import "go.uber.org/zap"
 
 // NewManager creates a MemoryManager based on the cloudConnection flag.
-// When cloudConnection is true (default), it uses the cloud-backed manager.
-// When false, it uses the local file-based manager.
 func NewManager(memoryRoot, apiKey string, cloudConnection bool, log *zap.Logger) MemoryManager {
 	if cloudConnection {
 		return NewCloudManager(memoryRoot, apiKey, log)
